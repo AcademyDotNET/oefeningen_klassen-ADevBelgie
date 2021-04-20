6de212162d3163f0005702b063e9c1aff70dcb13
Arthur Devresse <arthur.devresse@outlook.com>
2021-04-20 18:30:01 +0300

    bookmarks

--- Oefeningen arrays van klassen/Bookmark Manager/Program.cs
+++ Oefeningen arrays van klassen/Bookmark Manager/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bookmark_Manager
 {
@@ -6,7 +8,44 @@ namespace Bookmark_Manager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bookmark Manager\n");
+
+            List<BookMark> bookmarks = new List<BookMark>();
+            bookmarks.Add(new BookMark { Naam = "Bookmark 1", URL = @"https://www.hln.be/" });
+            bookmarks.Add(new BookMark { Naam = "Bookmark 2", URL = @"https://www.nieuwsblad.be/tag/het-laatste-nieuws" });
+            bookmarks.Add(new BookMark { Naam = "Bookmark 3", URL = @"https://www.youtube.com/" });
+            bookmarks.Add(new BookMark { Naam = "Bookmark 4", URL = @"https://www.youtube.com/watch?v=dQw4w9WgXcQ" });
+            bookmarks.Add(new BookMark { Naam = "Bookmark 5", URL = @"https://www.google.com/" });
+
+            foreach (BookMark bookmark in bookmarks)
+            {
+                Console.WriteLine($"{bookmark.Naam}: {bookmark.URL}");
+            }
+
+            int index = -2;
+            do
+            {
+                if (index == -1)
+                {
+                    Console.WriteLine($"That bookmark was not valid");
+                }
+                Console.WriteLine($"What bookmark do you want to open?");
+                string userRequest = Console.ReadLine();
+
+                index = bookmarks.FindIndex(b => b.Naam == userRequest);
+            } while (index == -1);
+            
+            bookmarks[index].OpenSite();
+            //bookmarks[0].OpenSite();
+            //string[,] favBookmarks = new string[5, 2]
+            //{
+            //    {"Bookmark 1",@"https://www.hln.be/"},
+            //    {"Bookmark 2",@"https://www.nieuwsblad.be/tag/het-laatste-nieuws"},
+            //    {"Bookmark 3",@"https://www.youtube.com/"},
+            //    {"Bookmark 4",@"https://www.youtube.com/watch?v=dQw4w9WgXcQ"},
+            //    {"Bookmark 5",@"https://www.google.com/"}
+            //};
+
         }
     }
 }
