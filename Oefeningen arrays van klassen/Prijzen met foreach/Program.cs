b0fddecf6638e249e01bf9b84b25c601ae52d6ff
Arthur Devresse <arthur.devresse@outlook.com>
2021-04-20 12:01:04 +0300

    oefeningen arrays van klassen

---
+++ Oefeningen arrays van klassen/Prijzen met foreach/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Prijzen_met_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prijzen met foreach\n");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            double[] prijzen = FillArray(20);
+
+            foreach (var prijs in prijzen)
+            {
+                if (prijs>=5)
+                {
+                    Console.WriteLine($"{prijs:C}");
+                }
+            }
+        }
+
+        private static double[] FillArray(int size)
+        {
+            double[] array = new double[size];
+            Random rand = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(0, 20);
+            }
+            return array;
+        }
+    }
+}
--- Oefeningen klassen - advanced/Sport simulator/Program.cs
+++ Oefeningen klassen - advanced/Sport simulator/Program.cs
@@ -27,3 +27,22 @@ namespace Sport_simulator
         }
     }
 }
+
+//Sport simulator
+//Haal je Sport klasse van vorig hoofdstuk boven en voeg volgende statische methode er aan toe (vervang het soort speler door de naam van jouw klasse. Mijn klasse noemde Waterpolospeler)
+
+//Schrijf een methode genaamd: static void SimuleerSpeler(Waterpolospeler testspeler)
+
+//(vervang dus Waterpolospeler door de klasse die je zelf hebt gemaakt)
+
+//De SimuleerSpeler-methode zal beide methoden van je klasse telkens 3x aanroepen m.b.v. een for-loop in de methode (dus in mijn geval 3x GooiBal en 3xWatertrappen)
+
+//Test je methode door 2 objecten aan te maken en telkens mee te geven als parameter.
+
+//Maak een tweede methode static void SimuleerWedstrijd(Waterpolospeler speler1, Waterpolospeler speler2)
+
+//Bij aanroep van de methode verschijnt er op het scherm wie van beide speler zou winnen als ze zouden spelen. Gebruik een random uitkomst om te bepalen over speler 1 of 2 wint. Toon op het scherm "Speler 1 wint." Gevolg door de aanroep van iedere methode van die speler.
+
+//Maak een derde methode static Waterpolospeler BesteSpeler(Waterpolospeler speler1, Waterpolospeler speler2)
+
+//Deze methode gaat ook random bepalen welke speler de beste is. Vervolgens geef je deze speler terug als resultaat. In de main roep je vervolgens iedere methode van dit object aan.
