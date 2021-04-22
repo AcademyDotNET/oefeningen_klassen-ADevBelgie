5a6a4012e7ccfcc0fcd814c0c285a02ff881035c
Arthur Devresse <arthur.devresse@outlook.com>
2021-04-22 09:20:43 +0300

    aanmaken Oefeningen overerving

---
+++ Oefeningen overerving/Ballspel met overerving/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ballspel_met_overerving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Oefeningen overerving/Het dierenrijk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Het_dierenrijk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
