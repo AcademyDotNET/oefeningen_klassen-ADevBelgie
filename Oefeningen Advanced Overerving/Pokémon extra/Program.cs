1863b96a93e18a018f8bc30f9f7af9231161c402
Arthur Devresse <arthur.devresse@outlook.com>
2021-04-23 10:43:45 +0300

    init Oefening advanced overerving

---
+++ Oefeningen Advanced Overerving/Pokémon extra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pokémon_extra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
