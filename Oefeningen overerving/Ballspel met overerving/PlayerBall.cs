bc2553f2d0b1cd890f7a5d74bf7fc8f431d08baf
Arthur Devresse <arthur.devresse@outlook.com>
2021-04-23 10:38:24 +0300

    ball

---
+++ Oefeningen overerving/Ballspel met overerving/PlayerBall.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ballspel_met_overerving
+{
+    class PlayerBall : Ball
+    {
+        public PlayerBall(int xin, int yin, int vxin, int vyin) : base(xin, yin, vxin, vyin)
+        {
+            drawChar = 'X';
+            drawColor = ConsoleColor.Green;
+        }
+
+        public void ChangeVelocity(ConsoleKeyInfo richting)
+        {
+            switch (richting.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    vy = -1;
+                    vx = 0;
+                    break;
+                case ConsoleKey.DownArrow:
+                    vy = 1;
+                    vx = 0;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    vx = -1;
+                    vy = 0;
+                    break;
+                case ConsoleKey.RightArrow:
+                    vx = 1;
+                    vy = 0;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- Oefeningen overerving/Ballspel met overerving/Program.cs
+++ Oefeningen overerving/Ballspel met overerving/Program.cs
@@ -6,7 +6,50 @@ namespace Ballspel_met_overerving
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.CursorVisible = false;
+            Console.WindowHeight = 40;
+            Console.WindowWidth = 70;
+            Ball[] balls = new Ball[2] 
+                {
+                    new Ball(4, 4, 1, 1), 
+                    new Ball(2, 15, 1, -1) 
+                };
+
+            PlayerBall player = new PlayerBall(10, 15, 0, 0);
+
+            int score = 0;
+
+            while (true)
+            {
+                foreach (var ball in balls)
+                {
+                    //Ball
+                    ball.Update();
+                    ball.Draw();
+                }
+                
+
+                //SpelerBall
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey();
+                    player.ChangeVelocity(key);
+                }
+
+                player.Update();
+                player.Draw();
+
+                //Check collisions
+                if (Ball.CheckHit(balls[0], player) || Ball.CheckHit(balls[1], player))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Gewonnen!");
+                    Console.WriteLine("score: {0}", score);
+                    Console.ReadLine();
+                }
+                System.Threading.Thread.Sleep(50);
+                score++;
+            }
         }
     }
 }
