b7b64588743c306d124ea377c2962d6fb26d64a0
Arthur Devresse <arthur.devresse@outlook.com>
2021-04-27 12:41:17 +0300

    magic  cards

---
+++ Oefeningen Polymorphisme/Magic/CreatureCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic
+{
+    class CreatureCard : Card
+    {
+        public int Health { get; set; }
+        public int Attack { get; set; }
+    }
+}
--- Oefeningen Polymorphisme/Magic/Program.cs
+++ Oefeningen Polymorphisme/Magic/Program.cs
@@ -6,7 +6,24 @@ namespace Magic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Magic\n");
+
+            var manager = new Manager();
+            manager.Cards.Add(new CreatureCard() { Health = 10, Attack =  10});
+            manager.Cards.Add(new SpellCard() { Damage = 10});
+            manager.Cards.Add(new LandCard());
+            manager.Cards.Add(new ArtifactCard());
+
+            if (manager.Cards[0] is CreatureCard)
+            {
+                var health = ((CreatureCard)manager.Cards[0]).Health;
+                Console.WriteLine(health);
+            }
+            if (manager.Cards[1] is SpellCard)
+            {
+                var effect = ((SpellCard)manager.Cards[1]).Damage;
+                Console.WriteLine(effect);
+            }
         }
     }
 }
