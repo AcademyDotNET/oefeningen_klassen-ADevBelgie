9317c3ffb214d03ca07ae375afccecf16c807140
Arthur Devresse <arthur.devresse@outlook.com>
2021-04-28 15:49:24 +0300

    readkey

--- Oefeningen Interfaces/Game/Program.cs
+++ Oefeningen Interfaces/Game/Program.cs
@@ -10,8 +10,32 @@ namespace Game
             Console.WriteLine("Game\n");
 
             SpeelVeld speelVeld = new SpeelVeld();
-
-            Console.WriteLine(speelVeld);
+            while (true)
+            {
+                
+                Console.SetCursorPosition(0,2);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine(speelVeld);
+                var input = Console.ReadKey();
+                switch (input.Key)
+                {
+                    case ConsoleKey.Z://vooruit
+                        break;
+                    case ConsoleKey.S://achteruit
+                        break;
+                    case ConsoleKey.Q://links
+                        break;
+                    case ConsoleKey.D://rechts
+                        break;
+                    case ConsoleKey.A: //schieten
+                        break;
+                    default:
+                        break;
+                }
+                Console.SetCursorPosition(2, 0);
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(speelVeld);
+            }
         }
     }
 }
