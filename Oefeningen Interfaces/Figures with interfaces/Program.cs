b54b1eca8f869c57528c1c7c6dc480cc9b8080b1
Arthur Devresse <arthur.devresse@outlook.com>
2021-04-28 12:12:59 +0300

    init

---
+++ Oefeningen Interfaces/Figures with interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Figures_with_interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Oefeningen Interfaces/Mapmaker afwerken/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mapmaker_afwerken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
