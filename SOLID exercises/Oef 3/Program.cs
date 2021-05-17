7c665dee1e502b25841ab8f327f23e3d348d862d
ADev <arthur.devresse@gmail.com>
2021-05-17 15:53:58 +0200

    SOLID oef 3

---
+++ SOLID exercises/Oef 3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oef_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public enum Kleuren
+    {
+        geel,
+        groen,
+        blauw,
+        all
+    }
+    public class Product
+    {
+        public string Naam { get; set; }
+        public Kleuren Kleur { get; set; }
+        public int Grootte { get; set; }
+    }
+    public class Filter
+    {
+        public List<Product> FilterMethod(List<Product> lijst, Kleuren kleur = Kleuren.all, int grootte = -1)
+        {
+            var tmp = new List<Product>(); //new is glue
+
+            if (grootte != -1 && kleur != Kleuren.all)//sort based on size && color
+            {
+                foreach (var p in lijst)
+                {
+                    if (p.Kleur == kleur && p.Grootte == grootte)
+                        tmp.Add(p);
+                }
+            }
+            else if (kleur != Kleuren.all)//sort based on color
+            {
+                foreach (var p in lijst)
+                {
+                    if (p.Kleur == kleur)
+                        tmp.Add(p);
+                }
+            }
+            else if (grootte != -1)//sort based on size
+            {
+                foreach (var p in lijst)
+                {
+                    if (p.Grootte == grootte)
+                        tmp.Add(p);
+                }
+            }
+            
+
+            return tmp;
+        }
+    }
+}
---
+++ SOLID exercises/oef 1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oef_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public abstract class Shape
+    {
+        public abstract double Opp();
+    }
+    class Circle : Shape
+    {
+        public int straal;
+        public Circle(int s)
+        {
+            straal = s;
+        }
+        public override double Opp()
+        {
+            return straal * straal * Math.PI;
+        }
+    }
+    class Square : Shape
+    {
+        public int lengte;
+        public Square(int l)
+        {
+            lengte = l;
+        }
+        public override double Opp()
+        {
+            return lengte * lengte;
+        }
+    }
+    class OppBerekenaar
+    {
+        List<Shape> lijst;
+        public OppBerekenaar()
+        {
+            lijst = new List<Shape>();
+        }
+        public void VoegToe(Shape s)
+        {
+            lijst.Add(s);
+        }
+        private double Sum()
+        {
+            double total = 0;
+            foreach (Shape s in lijst)
+            {
+                total += s.Opp();
+            }
+            return total;
+        }
+        public string Output()
+        {
+            return "<h1> Totale som = " + Sum() + " </h1> ";
+        }
+    }
+}
